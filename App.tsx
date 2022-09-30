eac38ba337d0a068c311f93e72cc8598466d8fd2
adefrol <adefrolx@gmail.com>
2022-09-30 19:44:57 +0800

    auth 2

--- App.tsx
+++ App.tsx
@@ -1,45 +1,94 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useRef } from 'react';
+import { KeyboardAvoidingView, KeyboardAvoidingViewBase, KeyboardAvoidingViewComponent, Platform, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Keyboard, TouchableOpacity } from 'react-native';
 import Input from './componets/Input';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.auth}>
-          Авторизация
-        </Text>
-        <Input></Input>
-      </View>
-    </View>
-  );
+const App = () => {
+
+
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.keyavoid}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+                    <View style={styles.main}>
+                        <Text style={styles.auth}>
+                            Авторизация
+                        </Text>
+                        <Input value='adefrolx@gmail.com' placeholder='E-mail' />
+                        <Input value='12345' placeholder='Пароль' />
+                        <TouchableOpacity style={{ marginTop: 10 }}>
+                            <Text style={{ color: '#ff941a', textDecorationLine: 'underline' }}>Забыли пароль?</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.enter}>
+                            <Text style={{
+                                ...Platform.select({
+                                    ios: {
+                                        lineHeight: 50
+                                    },
+                                    android: {}
+                                }),
+                                textAlign:'center',
+                                color: 'white'
+                            }} >Войти</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection:'column',
-    backgroundColor: '#000',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#000',
+        justifyContent: 'center',
+    },
 
-  main: {
-    height: '50%',
-    width: '100%',
-    backgroundColor: '#fff',
-    alignSelf:'flex-end',
-    position: 'absolute',
-    bottom:0,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
+    main: {
+        height: '50%',
+        width: '100%',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        bottom: 0,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
 
-  },
+    },
 
-  auth:{
-    textAlign:'center',
-    fontWeight:'800',
-    fontSize: 25,
-    marginTop: 15,
-    color:'#ff941a',
-  },
+    auth: {
+        textAlign: 'center',
+        fontWeight: '800',
+        fontSize: 25,
+        marginTop: 15,
+        color: '#ff941a',
+    },
+    keyavoid: {
+        flex: 1,
+    },
+
+    enter: {
+        width: 150,
+        height: 50,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: 'white',
+        backgroundColor: '#ff941a',
+        marginTop: 50,
+        borderRadius: 30,
+
+        shadowColor: "#d15102",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.4,
+        shadowRadius: 2.62,
+        elevation: 4,
+    }
 });
+
+export default App;
--- componets/Input.tsx
+++ componets/Input.tsx
@@ -1,15 +1,38 @@
-import { TextInput } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 
-export default function Input(){
-    return(
-        <TextInput style={styles.input}>
-            
+interface InputProps {
+    value?: string;
+    placeholder: string;
+}
+
+const Input = ({value, placeholder}: InputProps) => {
+    return (
+        <TextInput style={styles.input} returnKeyType={"next"} placeholder={placeholder} placeholderTextColor={'#ffbb6e'} >
+            {value}
         </TextInput>
     );
 
-    const styles = StyleSheet.create({
-        input: {
-            border
-        },
-    });
+
 };
+const styles = StyleSheet.create({
+    input: {
+        borderStyle: "solid",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+        backgroundColor: '#fff',
+        borderRadius: 30,
+        color: '#ff941a',
+        width: '80%',
+        height: 40,
+        marginTop: '10%',
+        textAlign: 'center',
+    },
+});
+
+export default Input;
